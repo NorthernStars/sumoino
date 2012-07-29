edef7a743c83ff172ac30cabc535879ef26c9123
Silver <silver.kuusik@gmail.com>
2012-07-29 15:59:31 +0300

    Update Sumorobot/Sumo.h

--- Sumorobot/Sumo.h
+++ Sumorobot/Sumo.h
@@ -1,20 +1,22 @@
 /*
-  Sumo.h - Sumorobot library for Arduino
-  Copyright (c) 2011 The Estonian Information Technology College.  All right reserved.
+Sumo.h - Sumorobot library for Arduino
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+Copyright (C) 2012 The Estonian Information Technology College Robotics Club
+Copyright (C) 2012 Silver Kuusik <silver.kuusik@gmail.com>
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #ifndef Sumo_h
