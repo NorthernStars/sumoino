94769056c110c24648b427678c1c33b448af0aff
Silver <silver.kuusik@gmail.com>
2012-08-18 14:45:49 +0300

    Update Sumorobot/examples/Opponent_EE/Opponent_EE.ino
    fixed type for sensor macros

--- Sumorobot/examples/Opponent_EE/Opponent_EE.ino
+++ Sumorobot/examples/Opponent_EE/Opponent_EE.ino
@@ -17,9 +17,9 @@ void setup()
 //                AEGLANE asemel saab kasutada, MAX_KIIRUS ning 0% kuni 100%) 
 void loop() 
 { 
-  if ( VASTANE_EES ) edasi();             // kui vastane on otse ees, liigume edasi 
-  else if ( VASTANE_VASAKUL ) vasakule(); // kui vastane on vasakul, liigume vasakule 
-  else if ( VASTANE_PAREMAL ) paremale(); // kui vastane on paremal, liigume paremale 
-  else stopp();                           // kui vastane pole anduritele leitav 
+  if ( VASTANE_EES ) edasi();           // kui vastane on otse ees, liigume edasi 
+  else if ( VASTANE_VASAK ) vasakule(); // kui vastane on vasakul, liigume vasakule 
+  else if ( VASTANE_PAREM ) paremale(); // kui vastane on paremal, liigume paremale 
+  else stopp();                         // kui vastane pole anduritele leitav 
 } 
  
