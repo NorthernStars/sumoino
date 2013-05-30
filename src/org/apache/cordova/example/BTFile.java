e300a54140a45e52ba639ada1933df9b64e0fd48
Saad Salman <saadsalmankhan@gmail.com>
2013-05-30 16:10:32 +0200

    added file writing

--- src/org/apache/cordova/example/BTFile.java
+++ src/org/apache/cordova/example/BTFile.java
@@ -1,8 +1,10 @@
 package org.apache.cordova.example;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.PrintWriter;
 
 public class BTFile 
 {
@@ -11,71 +13,105 @@ public class BTFile
 	{
 		path=file_path;
 	}
-	
-	
-	
+
+
+
 	//public class filedata
 	//{
-		public static void main(String[] args) throws IOException
-		{
-		String file_name="c:/Blink.hex";
+	public static byte[] main() 
+	{
+
+		String file_name= "C:/11.txt";	
+
+		BTFile data = new BTFile(file_name,false);
+		
+		System.out.println("text file written to ");
+		
+		file_name="/sdcard/Blink.hex";
 		//System.out.println("kala");
 		try
 		{
+			data.writeToFile("this is it");
 			BTFile file = new BTFile( file_name );
-			String[ ] aryLines = file.OpenFile( );
-			
-			System.out.println(file.OpenFile());
+			byte[ ] aryLines = file.OpenFile( );
+			return aryLines;
 		}
 		catch ( IOException e ) 
 		{
 			System.out.println( e.getMessage() );
+			return null;
 		}
-		
-		}
-//	}
-	
-	
-	
-	
-	
-	public String[] OpenFile() throws IOException
+
+	}
+	//	}
+
+
+
+
+
+	public byte[] OpenFile() throws IOException
 	{
-		
-		
-		
+
+
+
 		FileReader Fr= new FileReader(path);
 		BufferedReader textReader=new BufferedReader(Fr);
 		int numberOfLines = readLines( );
-		String[ ] textData = new String[numberOfLines];
-		
+		String textData = new String();
+
 		int i;
 
 		for (i=0; i < numberOfLines; i++) {
-		textData[ i ] = textReader.readLine();
-		System.out.println(textData[i]);
+			textData += textReader.readLine();
+			System.out.println(textData);
 		}
-		
+
 		textReader.close( );
-		
-		return textData;
-		
+
+		return textData.getBytes();
+
 	}
-	
+
 	int readLines() throws IOException
 	{
-		
-	FileReader file_to_read= new FileReader (path);
-	BufferedReader bf= new BufferedReader(file_to_read);
-	String aLine;
-	int numberOfLines=0;
-	while((aLine=bf.readLine()) != null)
-	{
+
+		FileReader file_to_read= new FileReader (path);
+		BufferedReader bf= new BufferedReader(file_to_read);
+		String aLine;
+		int numberOfLines=0;
+		while((aLine=bf.readLine()) != null)
+		{
 			numberOfLines++;
+		}
+		bf.close();
+		return numberOfLines;
+
 	}
-	bf.close();
-	return numberOfLines;
-	
+
+	private boolean append_to_file= false;
+
+
+
+
+	public BTFile( String file_path , boolean append_value ) 
+	{
+
+		path = file_path;
+		append_to_file = append_value;
+
+	}	
+
+	public void writeToFile( String textLine ) throws IOException 
+	{
+		FileWriter write = new FileWriter( path , append_to_file);
+		PrintWriter print_line = new PrintWriter( write );
+		print_line.printf( "%s" + "%n" , textLine);
+		print_line.close();
+
+
+
 	}
-	
+
+
+
 }
